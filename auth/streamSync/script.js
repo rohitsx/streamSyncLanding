7281a7eff9bf34eb153fe4ac230790d6ba275634
rohit <rohiitrb@gmail.com>
2024-11-29 12:38:43 +0530

    added script

---
+++ auth/streamSync/script.js
@@ -0,0 +1,38 @@
+const url = window.location.href;
+
+function getAuthToken() {
+  const objUrl = new URL(url);
+  return objUrl.searchParams.get("code");
+}
+
+async function sendAuthToken(code) {
+  const apiUrl = "http://0.0.0.0:8000/api/youtube-auth";
+  const data = JSON.stringify({ code });
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: data,
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const responseData = await response.json();
+    console.log("Success:", responseData);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+const code = getAuthToken();
+
+if (code) {
+  sendAuthToken(code);
+} else {
+  console.log("No auth token found in URL");
+}
